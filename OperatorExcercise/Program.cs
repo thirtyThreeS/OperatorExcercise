53e30702610ed945dc5a9eff294c5906aae5c051
thirtyThreeS <thaumasios@outlook.com>
2021-11-25 02:01:12 -0500

    initial commit

---
+++ OperatorExcercise/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Operator_Excercise
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"17/4 is remainder {remainder}"); ;
+            }
+        }
+
+    }
+}
